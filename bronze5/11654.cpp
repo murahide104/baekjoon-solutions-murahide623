0b9e3f3f66f01e135a3adebf3cfb0594e450fe52
Hidetoshi <hidetoshi0623@hotmail.com>
2023-11-03 09:39:24 +0900

    AC

---
+++ bronze5/11654.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    char c;
+    cin >> c;
+
+    int asciiValue = static_cast<int>(c);
+    cout << asciiValue << endl;
+}
