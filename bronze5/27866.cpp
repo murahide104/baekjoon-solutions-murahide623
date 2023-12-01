c184695d180491e57c13967b8cbcf8925495f82b
Hidetoshi <hidetoshi0623@hotmail.com>
2023-12-01 17:19:09 +0900

    AC

---
+++ bronze5/27866.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    string s;
+    int i;
+    cin >> s >> i;
+
+    cout << s.at(i-1) << endl;
+}
